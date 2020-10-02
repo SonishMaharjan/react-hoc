9c66200540144968cd476eb7c21442016e770378
sonish <sonishmzn95@gmail.com>
2020-10-02 09:24:28 +0545

    npm installed

--- src/withOnClick.js
+++ src/withOnClick.js
@@ -1,7 +1,9 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React from "react";
 
-export default BaseComponent => ({ children, onToggleClick, ...rest }) => (
+//This is anonymous function which takes BaseComponet as param
+//Nont ** BaseComponent is not a React Component. Its a parameter of a function
+export default (BaseComponent) => ({ children, onToggleClick, ...rest }) => (
   <BaseComponent {...rest}>
     {React.cloneElement(children, { onClick: onToggleClick })}
   </BaseComponent>
